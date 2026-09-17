5399511e7c5f27ae82c887e4422665608beba257
subhamkumar69 <subhamkumarnath18@gmail.com>
2026-09-17 12:27:57 +0530

    Update Exp 6 pro 1.py

--- Exp 6 pro 1.py
+++ Exp 6 pro 1.py
@@ -1,15 +1,12 @@
 def group_similar(matrix, rows, cols):
     elements = []
 
-    # Store all elements in a list
     for i in range(rows):
         for j in range(cols):
             elements.append(matrix[i][j])
 
-    # Sort the elements
     elements.sort()
 
-    # Create the second matrix
     result = []
     k = 0
 
@@ -22,8 +19,6 @@ def group_similar(matrix, rows, cols):
 
     return result
 
-
-# Input
 rows = int(input("Enter number of rows: "))
 cols = int(input("Enter number of columns: "))
 
@@ -34,10 +29,8 @@ for i in range(rows):
     row = list(map(int, input().split()))
     matrix.append(row)
 
-# Function call
 result = group_similar(matrix, rows, cols)
 
-# Output
 print("\nOriginal Matrix:")
 for row in matrix:
     print(*row)
